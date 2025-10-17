a45d9b5305e65ee73004a8f235a757f2d5783415
kaplannp <kaplannp@umich.edu>
2025-10-17 08:16:02 -0400

    added backtest scripts. work for chain, poa, bsw

--- gendp/kernel/poaV2/run.sh
+++ gendp/kernel/poaV2/run.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-num_thread_group=10
+num_thread_group=1
 num_threads=1
 
-for i in {0..9}
+for i in {0..0}
 do
   ./poa -datasets_path ../../../gendp-datasets -read_fasta ../../../gendp-datasets/poa/poa_$((i*num_threads+1)) -clustal tmp -hb blosum80_small.mat &
   P1=$!
--- gendp/scripts/chain_throughput.sh
+++ gendp/scripts/chain_throughput.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+INPUT_SIZE_CHAIN=$1
+INPUT_FILE=$2
+
 python3 scripts/chain_instruction_generator.py
 make clean && make -j
 cp sim sim_chain
 
-./sim_chain -k 4 -i ../gendp-datasets/c_elegans_40x.10k.in -n $1 -o chain_output.txt -s > chain_sim_results/chain_sim_result.txt
+./sim_chain -k 4 -i $INPUT_FILE -n $INPUT_SIZE_CHAIN -o chain_output.txt -s > chain_sim_results/chain_sim_result.txt
 python3 scripts/chain_check_correctness.py ../gendp-datasets/chain_output.txt chain_output.txt > chain_correctness.txt
 python3 scripts/chain_throughput.py chain_sim_results/chain_sim_result.txt chain_correctness.txt
