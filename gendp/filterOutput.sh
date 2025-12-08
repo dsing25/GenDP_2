38ac0382e3b350e86b8884d1f983887b7efc3c41
kaplannp <kaplannp@umich.edu>
2025-12-08 09:14:08 -0500

    clean up newlines in out

--- gendp/filterOutput.sh
+++ gendp/filterOutput.sh
@@ -21,7 +21,7 @@ grep -P "^main" out.txt | grep -oP '@\d+:[0-9a-f]+' | paste -sd' \n' - > Traces/
 grep -P "^main" out.txt | paste -sd' \n' - > Traces/fullMain.txt
 
 #strip out all traces. Just get raw output
-grep -Pv "^PE\[\d\]|^main" out.txt > Traces/out.txt
+grep -Pv "^PE\[\d\]|^main|^$" out.txt > Traces/out.txt
 cp out.txt Traces/fullOut.txt
 
 
